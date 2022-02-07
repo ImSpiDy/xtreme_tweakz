f2a5a328c0e9a9273a621b919475445cd18ce8e4
Ayush <90475639+ImSpiDy@users.noreply.github.com>
2022-02-08 02:59:48 +0530

    xtz: Import `Build & Workflow` Script
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ZIPNAME=Xtreme-TweakZ-x0.${buildNo}.zip
+
+function push() {
+curl -F document=@$1 "https://api.telegram.org/bot${bot_token}/sendDocument" \
+     -F chat_id="${chat_id}"  \
+     -F "disable_web_page_preview=true" \
+     -F "parse_mode=html" \
+     -F caption="`cat changelog.txt`"
+}
+
+zip -r9 "${ZIPNAME}" * -x build.sh -x *.github*
+
+push "${ZIPNAME}"
