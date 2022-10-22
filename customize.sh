8571ec23f6d08a568f60edffd6cf7b97ff4899f5
ImSpiDy <SpiDy2713@gmail.com>
2022-10-22 23:31:20 +0530

    xtz: Add XTZ Scripts
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- customize.sh
+++ customize.sh
@@ -8,3 +8,4 @@ unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 
 # Set Permission
 set_perm_recursive $MODPATH 0 0 0755 0644
+set_perm_recursive $MODPATH/system/bin 0 2000 0755 0755
--- service.sh
+++ service.sh
@@ -4,3 +4,6 @@ MODDIR=${0%/*}
 
 # Late Start To Prevent Conflicts
 sleep 69
+
+# Lets Start Loop
+xtz.loop
