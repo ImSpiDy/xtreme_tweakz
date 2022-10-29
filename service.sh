e1e6fe3b2cf6ab1b328c9f140b5420bba4647b28
ImSpiDy <SpiDy2713@gmail.com>
2022-10-29 14:08:19 +0530

    xtz: improve xtz log [2]
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- service.sh
+++ service.sh
@@ -5,6 +5,16 @@ MODDIR=${0%/*}
 # Late Start To Prevent Conflicts
 sleep 69
 
+xtz_log=/storage/emulated/0/Xtreme_TweakZ.log
+echo "- Xtreme TweakZ Executed Successfully" > $xtz_log
+echo "
+[*] Device : $(getprop ro.product.model) 
+[*] Android Version : $(getprop ro.system.build.version.release)
+[*] SOC : $(getprop ro.board.platform)
+[*] Kernel : $(uname -r)
+[*] SElinux Status : $(su -c getenforce) 
+" >> $xtz_log
+
 # Start OneGo Script
 xtz.onego
 
